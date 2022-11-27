6687bc6de30826b6dc0d6d32de9c0ef7be7b4670
Catunima <2009055@upy.edu.mx>
2022-11-26 20:48:19 -0600

    primera fila

--- main.py
+++ main.py
@@ -20,14 +20,17 @@ if option == "2": #Opcion para trabajar con integración bajo la curva
     intervalo = 1#variable para el inicio del intervalo
     intervalo_anterior = 0 #variable para el anterior
     x_med = 0.0
+    function = 0.0
+    area = 0.0
     intervalo_anterior = intervalo
     x = 0 #variable de x para la función
     intervalo = intervalo + 0.4
     x_med = (intervalo+intervalo_anterior)/2
-    
+    function = 1/2 * (intervalo)*(math.sin(intervalo)+1)
+    area =0.4*function
 
     print("x     |x(med)     |f(x)        |Area   ")
-    print("{}   |   {}".format(intervalo, x_med))
+    print("{}   |{}          |{}|{}      ".format(intervalo, x_med,function,area))
 
 if option == "3":
     print("======Integration by Trapecio======")
