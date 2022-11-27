fd2d5024758bb6461c7459c5ae49500807207a26
Catunima <2009055@upy.edu.mx>
2022-11-26 19:56:13 -0600

    condicinales primeros 2

--- main.py
+++ main.py
@@ -5,6 +5,14 @@
 print("welcome")
 menu = ""#variable para datos
 print("selecciona una option")
-print ("Newton Raphson")
-print("Integration under the curve")
-print("Integration by Trapecio")
+print ("1. Newton Raphson")
+print("2. Integration under the curve")
+print("3. Integration by Trapecio")
+
+option = input("Selecciona una opcion")#recibir datos
+
+if option == "1":
+    print("Newton Raphson")
+    print("ecuacion tal")
+if option == "2":
+    print("Integration under the curve")
